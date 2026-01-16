2906c852d59506d6a49ce50d7029b93cd05f3528
munishbp <146845753+munishbp@users.noreply.github.com>
2026-01-15 23:30:25 -0500

    HW0 start prep

---
+++ HW0.py
@@ -0,0 +1,22 @@
+import numpy as num
+import matplotlib as mat
+
+
+
+
+def g_distribution(samples:int):
+    num.random.normal(0)
+
+
+def scatterplot():
+
+
+
+
+
+
+
+def main():
+    g_distribution(100)
+    scatterplot()
+
