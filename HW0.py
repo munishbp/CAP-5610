1c82fedb75536319969bba0f078a3832c6f92275
munishbp <146845753+munishbp@users.noreply.github.com>
2026-01-16 15:18:30 -0500

    HW0 is finished

--- HW0.py
+++ HW0.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 
 def g_distribution(samples:int):
-    data: num.ndarray=num.random.multivariate_normal([0,0],[[1,0],[0,1]],samples)
+    data: num.ndarray=num.random.multivariate_normal([0,0],[[1,-0.5],[-0.5,1]],samples)
 
     plt.scatter(data[:,0],data[:,1])
 
